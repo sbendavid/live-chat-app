e30f21771aa65930c0517b604db997cf35ee9bc7
Ben-David <samuelbendavid01@gmail.com>
2023-10-19 22:40:14 +0100

    routing to the chat

--- server/routes/main.js
+++ server/routes/main.js
@@ -10,4 +10,19 @@ router.get("/chat", (req, res) => {
     res.render("chat");
 });
 
+router.get('/messages', (req, res) => {
+    Message.find({},(err, messages)=> {
+      res.send(messages);
+    });
+});
+
+ router.post('/messages', (req, res) => {
+    var message = new Message(req.body);
+    message.save((err) =>{
+      if(err)
+        sendStatus(500);
+      res.sendStatus(200);
+    });
+});
+
 module.exports = router
